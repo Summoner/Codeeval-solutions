120dd3eb0170a6ed9ea6a6688dac3fca6ff4beae
Summoner <andrtur@gmail.com>
2014-07-28 16:58:21 +0300

    2

--- Medium/CASH_REGISTER.pl
+++ Medium/CASH_REGISTER.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl -d
 use strict;
 use warnings;
 use Data::Dumper; 
@@ -12,6 +12,17 @@ open my $input, "D:\\Perl\\input.txt" || die "Can't open file: $!\n";
 
 
 my @list = ();
+my %coins_hash = (
+0.01 => "PENNY",
+0.05 => "NICKEL",
+0.10 => "DIME",
+0.25 => "QUARTER",
+0.50 => "HALF DOLLAR",
+1.00 => "ONE",
+2.00 => "TWO",
+5.00 => "FIVE",
+10.00 => "TEN",
+);
 while(<$input>){
 	
 	chomp;
@@ -24,22 +35,12 @@ close $input;
 foreach (@list){
 	
 	
-	
+	calc($_->[0],$_->[1]);
 	
 }
 
 
-my %coins_hash = (
-0.01 => "PENNY",
-0.05 => "NICKEL",
-0.10 => "DIME",
-0.25 => "QUARTER",
-0.50 => "HALF DOLLAR",
-1.00 => "ONE",
-2.00 => "TWO",
-5.00 => "FIVE",
-10.00 => "TEN",
-);
+
 
 
 
@@ -47,16 +48,37 @@ my %coins_hash = (
 sub calc{
 
 	my ($val1,$val2) = @_;
-	
+	my @result_coins = ();
 	print "ERROR\n" if ($val1 > $val2);
 	print "ZERO\n" if ($val1 == $val2);
 	
 	my @coins = sort {$a <=> $b}keys %coins_hash;
 	
-	my $money = $val1 - $val2;
-	
+	my $money = $val2 - $val1;
+	$money = sprintf '%.2f', $money;
+	$DB::single=2;
 	my @current_coins = grep{$_ <= $money}@coins;
 	
+	while($money > 0){
+		
+		foreach my $coin (reverse @current_coins){
+			
+			if ($money >= $coin){
+				
+				$money -= $coin;
+				push @result_coins, $coin;
+			}
+			
+		}
+		
+	}
+	
+	foreach(@result_coins){
+		
+		
+		print $coins_hash{$_},"\n";
+		
+		}
 }
 
 
