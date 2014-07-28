dc94a7a284c59cae11c5c542f8dc867324618040
Summoner <andrtur@gmail.com>
2014-07-28 10:56:22 +0300

    Add 186.pl

---
+++ Hard/186.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -d
+use strict;
+use warnings;
+use Data::Dumper; 
+use Benchmark;
+use Math::Trig;
+
+my $t0 = new Benchmark;
+
+open my $input, "D:\\Perl\\input.txt" || die "Can't open file: $!\n";
+# open my $result, ">D:\\Perl\\output1.txt" || die "Can't open file: $!\n";
+
+
+my @list = ();
+while(<$input>){
+	
+	chomp;
+	push @list,[split /\s+\|\s+/,$_];
+	
+}
+close $input;
+
+
+
+print "\n";
+my $t1 = new Benchmark;
+my $td = timediff($t1, $t0);
+print "the code took:",timestr($td),"\n";
