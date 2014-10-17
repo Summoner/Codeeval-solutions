de6f617b285d4f0e67fd4f254bf0068e4d86ec1e
Summoner <andrtur@gmail.com>
2014-10-17 10:15:07 +0300

    cash register solution added

--- Medium/CASH_REGISTER.pl
+++ Medium/CASH_REGISTER.pl
@@ -3,11 +3,11 @@ use strict;
 use warnings;
 use Data::Dumper; 
 use Benchmark;
-use Math::Trig;
+
 
 my $t0 = new Benchmark;
 
-open my $input, "D:\\Perl\\input.txt" || die "Can't open file: $!\n";
+open my $input, "/home/fanatic/Summoner/Codeeval-solutions/input.txt" || die "Can't open file: $!\n";
 # open my $result, ">D:\\Perl\\output1.txt" || die "Can't open file: $!\n";
 
 
@@ -22,7 +22,13 @@ my %coins_hash = (
 2.00 => "TWO",
 5.00 => "FIVE",
 10.00 => "TEN",
+20.00 => "TWENTY",
+50.00 => "FIFTY",
+100.00 => "ONE HUNDRED"
 );
+
+my @coins = grep{ $_ }sort{ $b<=>$a }keys %coins_hash;
+
 while(<$input>){
 	
 	chomp;
@@ -34,60 +40,39 @@ close $input;
 
 foreach (@list){
 	
-	
-	print calc($_->[0],$_->[1]),"\n";
+	my $price = $_->[0];
+    my $cash = $_->[1];
+
+    if ($price > $cash){
+         print "ERROR\n";
+         next;
+     }
+    if ($price == $cash){
+   
+        print "ZERO\n";
+        next;
+    }
+
+    my $change = sprintf("%.2f",$cash - $price);
+	print deep($change,[]),"\n";
 	
 }
 
+sub deep {
+    my	( $change, $res )	= @_;
 
+    return join ",",@$res if ( $change == 0 );
+    $change = sprintf("%.2f",$change); 
+    foreach my $coin ( @coins ) {
 
+        next if ($coin > $change);
+        $change -= $coin;
+        push @$res,$coins_hash{$coin};
+        last;
+    }
 
-
-
-
-sub calc{
-
-	my ($val1,$val2) = @_;
-	my @result_coins = ();
-	return "ERROR" if ($val1 > $val2);
-	return "ZERO" if ($val1 == $val2);
-	
-	my @coins = sort {$a<=>$b}keys %coins_hash;
-	
-	my $money = $val2 - $val1;
-	$money = sprintf ("%.2f", $money);
-	 
-	my @current_coins = grep{$_ <= $money}@coins;
-	#$DB::single=2;
-	while($money > 0){
-		
-		foreach my $coin(reverse @current_coins){
-			
-			
-			$money = sprintf ("%.2f", $money);
-			last if ($money == 0);
-			# print $money == $coin,"\n";
-			
-			if ($money >= $coin){
-				
-				$money -= $coin;
-				push @result_coins, $coin;
-			}
-			
-		}
-		
-	}
-	my @values = ();
-	foreach(sort {$b<=>$a}@result_coins){
-		
-		
-		push @values, $coins_hash{$_};
-		
-	}
-	
-	return join (",",@values);
-}
-
+    deep($change,$res);
+} ## --- end sub deep
 
 print "\n";
 my $t1 = new Benchmark;
