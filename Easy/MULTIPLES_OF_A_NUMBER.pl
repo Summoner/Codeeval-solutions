3d555598b3ddbf59e6f4bc71f31ebd7fe5f7035e
Summoner <andrtur@gmail.com>
2014-11-25 13:33:17 +0300

    Multiples of a number solution added

---
+++ Easy/MULTIPLES_OF_A_NUMBER.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+use Data::Dumper; 
+use Benchmark;
+use URI;
+my $t0 = new Benchmark;
+open my $input, "/home/fanatic/Summoner/Codeeval-solutions/input.txt" || die "Can't open file: $!\n";
+# open my $result, ">D:\\Perl\\output1.txt" || die "Can't open file: $!\n";
+
+
+
+my @list = (); 
+	 
+	while(<$input>){			
+	chomp;	
+	push @list,[split /,/,$_];
+	}
+close $input;
+
+#print Dumper \@list;
+foreach my $arr ( @list ) {
+
+    my $n = $arr->[1];
+
+    while( $n < $arr->[0] ){
+    
+        $n += $arr->[1];
+    }
+    print "$n\n";
+}
+my $t1 = new Benchmark;
+my $td = timediff($t1, $t0);
+print "the code took:",timestr($td),"\n";
