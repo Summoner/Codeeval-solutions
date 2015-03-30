655e7969f6036c1536b9dd4a1e4e250428a25296
Summoner <andrtur@gmail.com>
2015-03-30 13:37:30 +0300

    compressed sequence solution added

--- Hard/STRING_SUBSTITUTION.pl
+++ Hard/STRING_SUBSTITUTION.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 use utf8;
-use Data::Dumper; 
+use Data::Dumper;
 
 open my $input, "/home/fanatic/Summoner/Codeeval-solutions/input.txt" || die "Can't open file: $!\n";
 # open my $result, ">D:\\Perl\\output.txt" || die "Can't open file: $!\n";
@@ -10,23 +10,21 @@ open my $input, "/home/fanatic/Summoner/Codeeval-solutions/input.txt" || die "Ca
 my @list = ();
 
 while(<$input>){
-	
+
 	chomp;
 	push @list,[split /;/,$_];
-	
+
 }
 close $input;
-    
+
 foreach my $arr_ref (@list) {
 
     print find_final_string($arr_ref->[0],$arr_ref->[1]);
 }
 
-
-
 sub find_final_string {
     my	( $string,$replacement )	= @_;
-    
+
     my @replacements = split /,/,$replacement;
     my $finalString = "";
     my %hash = ();
@@ -34,12 +32,10 @@ sub find_final_string {
     $hash{0} = "b";
     my %rhash = reverse %hash;
 
-    
     for ( my $i = 0;$i < scalar @replacements;$i+=2) {
        my $inp =  $replacements[$i];
        my @repl = split //,$replacements[$i+1];
        my $repl = "";
-
 
        foreach (@repl) {
            $repl .= $hash{$_};
@@ -47,7 +43,7 @@ sub find_final_string {
         $string =~ s/$inp/$repl/g;
      }
     my @str_array = split //,$string;
-    
+
 
     foreach  (@str_array) {
 
