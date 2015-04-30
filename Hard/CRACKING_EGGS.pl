048eb9ff143c3d208356165f66bd0db518c5089c
Summoner <andrtur@gmail.com>
2015-04-30 18:56:28 +0300

    The cracking eggs solution (not my)

--- Hard/CRACKING_EGGS.pl
+++ Hard/CRACKING_EGGS.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl -d
 use strict;
 use warnings;
 use Data::Dumper;
@@ -14,16 +14,41 @@ my @list = ();
 
 	while(<$input>){
     	chomp;
-
+       push @list, [split /\s+/,$_];
 	}
 close $input;
 
 
+foreach my $arr ( @list ) {
+
+    print calc( $arr->[0],$arr->[1] ),"\n";
+}
 
 
+sub count {
+    my	( $eggs,$dropsCount )	= @_;
 
+    return 0 if ( $eggs == 0 );
 
+    my $result = 0;
+    for ( my $i=0; $i < $dropsCount; $i++ ) {
 
+        $result += count($eggs - 1,$i) +1;
+    }
+    return $result;
+} ## --- end sub count
+
+sub calc {
+    my	( $eggs,$floors )	= @_;
+
+    my $dropsCount = 0;
+    while ( count( $eggs, $dropsCount ) < $floors ){
+
+        $dropsCount++;
+
+    }
+    return $dropsCount;
+} ## --- end sub calc
 
 
 my $t1 = new Benchmark;
