e10de5856fd81fb1ff19a1bbe0dbc940e0211cef
Summoner <andrtur@gmail.com>
2015-08-05 14:08:38 +0300

    PENULTIMATE WORD solution added

---
+++ Easy/PENULTIMATE_WORD.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+use Data::Dumper;
+use Benchmark;
+
+my $t0 = new Benchmark;
+
+open my $input, "/home/fanatic/Summoner/Codeeval-solutions/input.txt" || die "Can't open file: $!\n";
+#open my $result, ">D:\\Perl\\output1.txt" || die "Can't open file: $!\n";
+
+my @list = ();
+
+while(<$input>){
+    chomp;
+    push @list,[split /\s+/,$_];
+}
+close $input;
+
+foreach my $arr ( @list ) {
+    print "$arr->[ $#{$arr}-1 ]\n";
+}
+
+
+my $t1 = new Benchmark;
+my $td = timediff($t1, $t0);
+print "the code took:",timestr($td),"\n";
