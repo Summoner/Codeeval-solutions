a0ac204f5f4146a716b1b2542bbe1fd8294c2232
Summoner <andrtur@gmail.com>
2015-09-08 14:49:25 +0300

    STRING SUBSTITUTION solution added

--- Hard/STRING_SUBSTITUTION.pl
+++ Hard/STRING_SUBSTITUTION.pl
@@ -19,10 +19,10 @@ close $input;
 
 foreach my $arr_ref (@list) {
 
-    print find_final_string($arr_ref->[0],$arr_ref->[1]);
+    print FindFinalString($arr_ref->[0],$arr_ref->[1]);
 }
 
-sub find_final_string {
+sub FindFinalString {
     my	( $string,$replacement )	= @_;
 
     my @replacements = split /,/,$replacement;
@@ -32,28 +32,25 @@ sub find_final_string {
     $hash{0} = "b";
     my %rhash = reverse %hash;
 
-    for ( my $i = 0;$i < scalar @replacements;$i+=2) {
-       my $inp =  $replacements[$i];
-       my @repl = split //,$replacements[$i+1];
-       my $repl = "";
+    for ( my $i = 0;$i < scalar @replacements;$i += 2 ){
+        my $inp =  $replacements[$i];
+        my @repl = split //,$replacements[$i+1];
+        my $repl = "";
 
-       foreach (@repl) {
-           $repl .= $hash{$_};
-       }
+        foreach (@repl) {
+            $repl .= $hash{$_};
+        }
         $string =~ s/$inp/$repl/g;
-     }
+    }
     my @str_array = split //,$string;
-
 
     foreach  (@str_array) {
 
         if (defined $rhash{$_}){
-
             $finalString .= $rhash{$_};
-
         }else{
             $finalString .= $_;
         }
     }
     return $finalString;
-} ## --- end sub find_final_string
+} ## --- end sub FindFinalString
